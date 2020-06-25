2a1ce2afb1eee246a9eed51e893012b917036d51
Anton <anton.holubnychyi@gmail.com>
2020-06-25 17:53:08 +0300

    add gitignore and init my branch to work with appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class CharacterScreen extends StatefulWidget {
+  @override
+  AppState createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<CharacterScreen> {
+  onButtonTap(Widget page) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (BuildContext context) => page));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
